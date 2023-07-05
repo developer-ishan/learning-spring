d88f6de6c8a235c1811414ebfbc0df5fa87f3a61
developer-ishan <62565307+developer-ishan@users.noreply.github.com>
2023-07-06 01:28:16 +0530

    loosely coulpling the gamerunner and the games using a game comsole interface

--- learn-spring-framework/src/main/java/com/developerishan/learnspringframework/AppGamingBasicJava.java
+++ learn-spring-framework/src/main/java/com/developerishan/learnspringframework/AppGamingBasicJava.java
@@ -2,16 +2,16 @@ package com.developerishan.learnspringframework;
 
 import com.developerishan.learnspringframework.game.GameRunner;
 import com.developerishan.learnspringframework.game.MarioGame;
+import com.developerishan.learnspringframework.game.SuperContraGame;
 
 public class AppGamingBasicJava {
 
 	public static void main(String[] args) {
 		
 		var marioGame = new MarioGame();
-		var gameRunner = new GameRunner(marioGame);
+		var superContraGame = new SuperContraGame();
+		var gameRunner = new GameRunner(superContraGame);
 		gameRunner.run();
-		
-				
 				
 	}
 
