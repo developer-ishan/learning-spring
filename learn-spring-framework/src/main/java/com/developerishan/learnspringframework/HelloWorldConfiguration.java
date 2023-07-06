9fb80049d7a8bd1769cad7cbd78c5c833d678c0f
developer-ishan <62565307+developer-ishan@users.noreply.github.com>
2023-07-07 01:54:03 +0530

    Manage custon objects with the spring beans.

--- learn-spring-framework/src/main/java/com/developerishan/learnspringframework/HelloWorldConfiguration.java
+++ learn-spring-framework/src/main/java/com/developerishan/learnspringframework/HelloWorldConfiguration.java
@@ -3,6 +3,8 @@ package com.developerishan.learnspringframework;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+record Person (String name, int age) {};
+record Address(String firstLine, String city) {};
 @Configuration
 public class HelloWorldConfiguration {
 	
@@ -10,4 +12,21 @@ public class HelloWorldConfiguration {
 	public String name() {
 		return "Ishan";
 	}
+	
+	@Bean
+	public int age() {
+		return 15;
+	}
+	
+	@Bean 
+	public Person person() {
+		var person = new Person("Ishan", 21);
+		return person;
+	}
+	
+	@Bean
+	public Address address() {
+		return new Address("MNNIT Malviya Hostel", "Prayagraj");
+	}
+	
 }
