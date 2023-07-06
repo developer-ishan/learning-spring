2d72df81b843f6def92cddd557d02e1b3eab96bd
developer-ishan <62565307+developer-ishan@users.noreply.github.com>
2023-07-07 01:33:37 +0530

    HelloWorld Spring Beans and config program

--- learn-spring-framework/src/main/java/com/developerishan/learnspringframework/App01GamingBasicJava.java
+++ learn-spring-framework/src/main/java/com/developerishan/learnspringframework/App01GamingBasicJava.java
@@ -5,14 +5,17 @@ import com.developerishan.learnspringframework.game.MarioGame;
 import com.developerishan.learnspringframework.game.PacmanGame;
 import com.developerishan.learnspringframework.game.SuperContraGame;
 
-public class AppGamingBasicJava {
+public class App01GamingBasicJava {
 
 	public static void main(String[] args) {
 		
 		var marioGame = new MarioGame();
 		var superContraGame = new SuperContraGame();
-		var pacmanGame = new PacmanGame();
+		var pacmanGame = new PacmanGame(); //1: Object Creation
 		var gameRunner = new GameRunner(pacmanGame);
+		//2: Object Creation + wiring of dependencies
+		// i.e. game runner needs an object of Game to run
+		// Game is the dependency of GameRunner
 		gameRunner.run();
 				
 	}
---
+++ learn-spring-framework/src/main/java/com/developerishan/learnspringframework/App02HelloWorldSpring.java
@@ -0,0 +1,24 @@
+package com.developerishan.learnspringframework;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import com.developerishan.learnspringframework.game.GameRunner;
+import com.developerishan.learnspringframework.game.MarioGame;
+import com.developerishan.learnspringframework.game.PacmanGame;
+import com.developerishan.learnspringframework.game.SuperContraGame;
+
+public class App02HelloWorldSpring {
+
+	public static void main(String[] args) {
+		
+		//1: Launch a Spring Context
+		var context = new AnnotationConfigApplicationContext(HelloWorldConfiguration.class);
+		//2: Configure the things that we want Spring to manage- @Configuartion
+		//HelloWorldConfiguration - @Configuration
+		//name - @Bean
+		
+		//3: Retrieving Beans managed by Spring
+		System.out.println(context.getBean("name"));
+	}
+
+}
